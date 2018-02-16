fb33a091f038c980d813bbfdb4d0a7c4189ce530
xiaochaohit <wangchao@hitee.net>
2018-02-16 21:34:40 +0800

    提交第二章部分代码

---
+++ chapter2/test/test_space.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	long a;
+	unsigned long b;
+	int c;
+	unsigned int d;
+	std::cout << sizeof(a) <<std::endl;
+	std::cout << sizeof(b) <<std::endl;
+	std::cout << sizeof(c) <<std::endl;
+	std::cout << sizeof(d) <<std::endl;
+
+	return 0;
+}
