01f60dff15dd5dfca6da98c07f3fc461b933d2d9
xiaochaohit <wangchao@hitee.net>
2018-02-16 22:22:44 +0800

    提交名次排序算法

---
+++ chapter2/test/sortByracking.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+//按名次排序，递增数列
+
+template <typename T>
+void Racking(T &list, T &rack)
+{
+	int n = sizeof(list)/sizeof(list[0]);
+	for(int i = 0; i < n; i++)	//初始化名次
+	{
+		rack[i] = 0;
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j<i; j++)
+			if(list[i] >= list[j]) rack[i]++;
+			else rack[j]++;
+	}
+
+}
+
+template <typename T>
+void sortNum(T list[], T rack[], int n)
+{//交换位置
+	T *tempList = new T [n];
+
+	for(int i = 0; i < n; i++)
+		tempList[rack[i]] = list[i];
+	
+	for(int i = 0; i < n; i++)
+		list[i] = tempList[i];
+}
+int main()
+{
+	int n = 5;
+	int list[5] = {4, 3, 9, 3, 7};
+	int rack[5] = {0, 0, 0, 0, 0};
+	Racking(list, rack);	//获得名次
+	sortNum(list, rack, n);
+	for(int i = 0; i<sizeof(list)/sizeof(list[0]); i++)
+		std::cout << list[i] <<" ";
+	std::cout << std::endl;
+	return 0;
+}
