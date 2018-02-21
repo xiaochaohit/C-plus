dc0925d20b15b64e0e2f7fe5803eea9bfe4fb2e7
xiaochaohit <wangchao@hitee.net>
2018-02-21 11:47:18 +0800

    更新至5.3.3

--- chapter5/test/5-3arrayList.cpp
+++ chapter5/test/5-3arrayList.cpp
@@ -3,6 +3,7 @@
 #include <sstream>	//包含ostringstream
 #include <algorithm>//包含find函数	
 #include <iterator>//ostream_iterator
+#include <string>
 //类arrayList
 
 
@@ -24,7 +25,7 @@ public:
 		//删除索引为theIndex的元素
 	virtual void insert(int theIndex, const T& theElement) = 0;
 		//在 theIndex位置插入元素theElement
-	virtual void output(std::ostream& out) const = 0;
+	virtual void output() const = 0;
 
 };
 
@@ -42,8 +43,7 @@ public:
 	int indexOf(const T &theElement) const;
 	void erase (int theIndex);
 	void insert(int theIndex, const T &theElement);
-	void output(std::ostream& out) const;
-
+	void output() const;
 	int capacity() const {return arrayLength;}
 
 protected:
@@ -57,8 +57,51 @@ int main()
 {
 	linearList<double> *x = new arrayList<double>(10);		//x为抽象类linearList的指针，则*x为linearList的一个实例
 	arrayList<int> y(2), z;
-	//首先(arrayList<double> *)将x强制转化为派生类arrayList的指针，然后再用解释符*，使其成为一个实例，则可将此实例传给构造函数（复制函数））
+	arrayList<double> w(*((arrayList<double> *)x));
 
+	//首先(arrayList<double> *)将x强制转化为派生类arrayList的指针，然后再用解释符*，使其成为一个实例，则可将此实例传给构造函数（复制函数））
+/*	//验证构造函数
+	std::cout << x -> arrayLength << " "
+		<< y.arrayLength << " "
+		<< z.arrayLength <<std::endl;*/
+	std::cout << "***************************************************************" << std::endl;
+	//验证构造函数
+	std::cout << "the capacity of y,z and w :"
+			//<< x->capacity() << " "			//x为抽象类linearList的指针，不能访问派生类中的保护变量
+			<< y.capacity() << " "
+			<< z.capacity() << " "
+			<< w.capacity() << std::endl;
+	//验证empty函数
+	std::cout << "the four array x,y,z and w is empty?" << std::endl;
+	if(x ->empty()) {std::cout << "is empty"<< std::endl;}
+	if(y.empty()) {std::cout << "is empty"<< std::endl;} 
+	if(z.empty()) {std::cout << "is empty" << std::endl;}
+
+	//验证size()函数
+	std::cout << "the size of x,y,z and w :" 
+		<< x->size() << " "
+		<< y.size() << " "
+		<< z.size() << std::endl;
+
+	//验证insert函数和indexOf函数
+	x -> insert(0,0);
+	x -> insert(0,1.1);
+	x -> insert(0,2.2);
+	x -> insert(0,3);
+	std::cout << x -> indexOf(2.2) << std::endl;
+
+	//验证erase函数和get函数
+	x -> erase(1);
+	std::cout << x -> get(1) << std::endl;
+
+	//验证outpu函数以及重载
+	y.insert(0,4);
+    y.insert(0,5);
+    y.insert(0,6);
+    y.insert(0,7);
+    y.output();
+    if(y.empty()) {std::cout << "is empty"<< std::endl;} 
+    
 	return 0;
 }
 
@@ -81,13 +124,16 @@ arrayList<T>::arrayList(const arrayList<T>& theList)
 {//复制构造函数
 	arrayLength = theList.arrayLength;
 	listSize = theList.listSize;
-	std::copy(theList.element, theList.element + arrayLength, element);
+	element = new T [arrayLength];
+	//std::cout << theList.element << std::endl;
+	//std::cout << arrayLength << std::endl;
+	std::copy(theList.element, theList.element + listSize, element);
 }
 
 template <typename T>
 void arrayList<T>::checkIndex(int theIndex) const
 {
-	if(theIndex <0 || theIndex >=listSize)
+	if(theIndex <0 || theIndex >listSize)
 	{
 		std::ostringstream s;
 		s <<"index = " << theIndex <<"size = " << listSize;
@@ -154,18 +200,17 @@ void arrayList<T>::insert(int theIndex, const T& theElement)
 }
 
 template<typename T>
-void arrayList <T> ::output(std::ostream& out) const
+void arrayList <T> ::output() const
 {
-	std::copy(element, element + listSize, std::ostream_iterator <T>(std::cout," "));
-}
-
-template <typename T>
-std::ostream& operator << (std::ostream& out, const arrayList<T>& x)
+	/*std::string s;
+	std::copy(element, element + listSize, std::ostream_iterator <T>(s," "));
+	std::cout << s << std::endl;*/
+	for (int i = 0; i < listSize; i++)
 	{
-		x.output(out);
-		return out;
+		std::cout << *(element+i) << " ";
 	}
-
+		std::cout << std::endl;
+}
 
 
 
