1c083f28253c95f281fa7f87b7cfa4d2eb49941c
xiaochaohit <wangchao@hitee.net>
2018-03-12 22:23:20 +0800

    增加第五章迭代器函数

---
+++ chapter5/test/5-9iterator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stddef.h> 	//包含ptrdiff_t ，用于存储两个指针减法的操作结果
+//#include <iterator.h>
+
+template <typename iterator>
+void copy(iterator start, iterator end, iterator to)
+{//从[start,end]复制到[to,to+end-start]
+	while(start != end)
+	{
+		*to = *start;
+		to++;
+		start++;
+	}
+}
+
+template <typename T>
+class iterator
+{
+public:
+	//
+	typedef std::bidirectional_iterator_tag iterator_category;
+	typedef T value_type;
+	typedef ptrdiff_t difference_type;
+	typedef T* pointer;
+	typedef T& reference;
+
+	iterator();
+	~iterator();
+	
+};
+
+int main()
+{
+	int testIn[] = {1,2,3,4,5},testOut[5];
+	copy(testIn, testIn+(sizeof(testIn)/sizeof(testIn[0])), testOut);
+	
+	for(int i = 0; i < sizeof(testIn)/sizeof(testIn[0]); i++)
+		std::cout << testOut[i] << std::endl;
+
+	return 0;
+}
