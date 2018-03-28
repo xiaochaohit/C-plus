162b8c2e791f2f397f79c97c382b38fa0110d655
XiaoC_hit <wangchao@hitee.net>
2018-03-29 00:09:43 +0800

    Update the constructed function

--- chapter5/test/6-1linearList chain.cpp
+++ chapter5/test/6-1linearList chain.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "linearList.h"
 #include "chain.h"
-
+#include "../../官方答案及解决方法网址/codes/myExceptions.h"
 using namespace std;
 
 
--- chapter5/test/chain.h
+++ chapter5/test/chain.h
@@ -3,23 +3,26 @@
 #ifndef chain_
 #define chain_
 #include <iostream>
+#include <sstream>
+#include <string>
 #include "linearList.h"
+#include "../../官方答案及解决方法网址/codes/myExceptions.h"
 
 using namespace std;
 
 template<typename T>
-struct chainNoed
+struct chainNode
 {
 	T element;
-	chainNoed<T> *next;
+	chainNode<T> *next;
 
 	//ADT
-	chainNoed() {}
-	chainNoed(const T& element)
+	chainNode() {}
+	chainNode(const T& element)
 	{
 		this->element = element;
 	}
-	chainNoed(const T& element, chainNoed<T> *next)
+	chainNode(const T& element, chainNode<T> *next)
 	{
 		this->element = element;
 		this->next = next;
@@ -30,25 +33,65 @@ template <typename T>
 class chain : public linearList<T>
 {
 public:
-	chain(int initialCapacity = 0);
+	chain(int initialCapacity = 10);
 	chain(const chain<T> &);
 	~chain();
 
 	//ADT
 	bool empty() const {return listSize == 0;}
 	int size() const {return listSize;}
-	T& get(int theindex) const;
+	T& get(int theIndex) const;
 	int indexof(const T& theElement) const;
-	void erase(int theindex) const;
-	void insert(int theindex, const T& theElement) const;
+	void erase(int theIndex);
+	void insert(int theIndex, const T& theElement);
 	void output(ostream &out) const;
 
 protected:
-	void checkIndex(int &theindex) const;
-	chainNoed<T> *firstNode;
+	void checkIndex(int &theIndex) const;
+	chainNode<T> *firstNode;
 	int listSize;
 	
 };
+
+template <typename T>
+chain<T>::chain(int initialCapacity)
+{
+	if(initialCapacity < 1)
+	{
+		ostringstream s;
+		s << "Initial capacity = " << initialCapacity << "Must be > 0";
+		throw illegalParameterValue(s.str());
+	}
+	firstNode = NULL;
+	listSize = 0;
+}
+
+template <typename T>
+chain<T>::chain(const chain<T> & theList)
+{
+	listSize = theList.listSize;
+	if(0 == listSize)
+	{
+		firstNode = NULL;
+		return;
+	}
+	chainNode<T> *sourceNode = theList.firstNode;
+	firstNode = new chainNode<T>(sourceNode -> element);
+	sourceNode = sourceNode -> next;
+	chainNode<T> *targetNode = firstNode;
+
+	while(sourceNode -> next != NULL)
+	{
+		targetNode = new chainNode<T>(sourceNode->element);
+		sourceNode = sourceNode -> next;
+		targetNode = targetNode -> next;
+	}
+
+
+}
+
+
+
 
 
 
