6138fba28dc411ac5cfcabbff5f244c913cc8bc5
XiaoC_hit <wangchao@hitee.net>
2018-03-29 08:26:48 +0800

    add the function get&checkIndex of the programm 6-1

--- chapter5/test/chain.h
+++ chapter5/test/chain.h
@@ -3,10 +3,10 @@
 #ifndef chain_
 #define chain_
 #include <iostream>
-#include <sstream>
+#include <sstream>		//including the ostringstream 
 #include <string>
 #include "linearList.h"
-#include "../../官方答案及解决方法网址/codes/myExceptions.h"
+#include "../../官方答案及解决方法网址/codes/myExceptions.h" //use the Exceptions Example
 
 using namespace std;
 
@@ -56,6 +56,7 @@ protected:
 template <typename T>
 chain<T>::chain(int initialCapacity)
 {
+	//function of initialing the chainNode
 	if(initialCapacity < 1)
 	{
 		ostringstream s;
@@ -64,21 +65,27 @@ chain<T>::chain(int initialCapacity)
 	}
 	firstNode = NULL;
 	listSize = 0;
+	//使用链表构造线性表时，其实不需要声明其大小，但是为了保持函数的一致性，也定义initialCapacity变量，但不去实际使用
 }
 
 template <typename T>
 chain<T>::chain(const chain<T> & theList)
 {
+	//copy the input chain
 	listSize = theList.listSize;
 	if(0 == listSize)
 	{
 		firstNode = NULL;
 		return;
 	}
-	chainNode<T> *sourceNode = theList.firstNode;
-	firstNode = new chainNode<T>(sourceNode -> element);
-	sourceNode = sourceNode -> next;
-	chainNode<T> *targetNode = firstNode;
+
+
+	chainNode<T> *sourceNode = theList.firstNode;			//use a parament save the input firstNode
+	firstNode = new chainNode<T>(sourceNode -> element);	//assignment the first input element to the firstNode
+	sourceNode = sourceNode -> next;						
+	chainNode<T> *targetNode = firstNode;					//use the targetNode to save the rest element
+	//by now, the adress of targetNode->next is same as the adress of firstNode->next.
+	//So the firtNode can visit all the element of the targetNode chain
 
 	while(sourceNode -> next != NULL)
 	{
@@ -87,12 +94,65 @@ chain<T>::chain(const chain<T> & theList)
 		targetNode = targetNode -> next;
 	}
 
+}
+
+template <typename T>
+chain<T>::~chain()
+{
+	while(firstNode->next != NULL)
+	{
+		chainNode<T> * nextNode = firstNode->next;	//Save the chain pointer
+		delete firstNode;					
+		firstNode = nextNode;
+	}
+
+}
+
+template<typename T>
+T& chain<T>::get(int theIndex) const
+{
+	//check the index
+	int i = 0;
+	chainNode<T> *getNode = firstNode;
+	while(i < theIndex)
+	{
+		getNode = getNode->next;
+		i++;
+	}
+
+	return getNode->element;
+}
+
+template<typename T>
+void chain<T>::checkIndex(int &theIndex) const
+{
+	if(theIndex > listSize || theIndex < 0)
+	{
+		ostringstream s;
+		s << "theIndex is out of the range of listSize" << endl;
+		throw illegalIndex( s.str() );
+	}
 
 }
 
 
 
-
-
-
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
